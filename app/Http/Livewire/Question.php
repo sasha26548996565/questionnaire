f11ce76f42289199efa38a0189d7f87e6f2c45f0
sasha <sashapozhidaev07@example.com>
2022-08-13 15:49:50 +0600

    index page

--- app/Http/Livewire/Question.php
+++ app/Http/Livewire/Question.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Question extends Component
 {
-    public function render()
+    public function render(): View
     {
-        return view('livewire.question');
+        return view('livewire.question')->extends('layouts.app');
     }
 }
--- resources/views/livewire/question.blade.php
+++ resources/views/livewire/question.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="container container-quiz mt-sm-5 my-1">
+    <div class="question ml-sm-5 pl-sm-5 pt-2">
+        <form>
+            <div class="py-2 h5"><b>ВОПРОС</b></div>
+            <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+                <label class="options">ОТВЕТ
+                    <input type="radio" name="radio" value="1"><span class="checkmark"></span>
+                </label>
+            </div>
+            <div class="d-flex align-items-center pt-3 justify-content-center">
+                <div class="ml-auto mr-sm-5">
+                    <button class="btn btn-success">Следующий</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
